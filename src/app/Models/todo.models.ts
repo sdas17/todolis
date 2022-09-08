41636565e51fb07d8febc52c5664307c2180c14b
sdas1 <sdas1@miraclesoft.com>
2022-09-08 16:19:26 +0530

    add array

---
+++ src/app/Models/todo.models.ts
@@ -0,0 +1,9 @@
+//export to give permiisson to any declartion
+//which will acess out side this file 
+
+export interface ITodo{
+    task:string;
+    createdon:Date;
+
+}
+//orm-pojo-modal-response
--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ITodo } from '../Models/todo.models';
 
 @Component({
   selector: 'app-todolist',
@@ -7,12 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodolistComponent implements OnInit {
 
+  //defince a blank array
+  items:ITodo[]=[];
   constructor() { }
   taskName: string = '';
   ngOnInit(): void {
   }
   handleAdd(tName: string) {
     this.taskName = tName
-
+//check duplicate
+//add the items
+this.items.push({
+  task:this.taskName,
+  createdon: new Date()
+});
+console.log(this.items)
   }
 }
